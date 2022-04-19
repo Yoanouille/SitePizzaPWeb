88a569c17761851b7ebb58d889b9ee9b19e07a65
Alexandre LEYMARIE <alexandre.j.leymarie@gmail.com>
2022-04-19 22:24:01 +0200

    Gestion des menus dans le panier

--- public/script.js
+++ public/script.js
@@ -1,5 +1,49 @@
 let panier = [];
 
+
+function gen_elt(elt){
+    let s =
+     '<li class="list-group-item d-flex justify-content-between align-items-center">'
+    +   '<div>'+elt.name;
+            if(elt.choice !== undefined){
+                s+=' <i class="bi-info-circle" data-toggle="tooltip" data-placement="top" title="'+elt.choice+'"></i>'
+            }
+    s +='</div><div><span class="badge badge-secondary">'+(elt.price)+'€</span> <span class="badge badge-primary badge-pill badge-success">'+elt.number+'</span></div>'
+    +'</li>';
+    return s;
+}
+
+function gen_menu_elt(elt){
+    let s =
+     '<div class="dropdown-item">'+elt.name;
+            if(elt.choice !== undefined){
+                s+=' <i class="bi-info-circle" data-toggle="tooltip" data-placement="top" title="'+elt.choice+'"></i>'
+            }
+    s +=' <span class="badge badge-primary badge-pill badge-success">'+elt.number+'</span>'
+    +'</div>';
+    return s;
+}
+
+function gen_menu(menu){
+    let s =
+     '<li class="list-group-item d-flex justify-content-between align-items-center">'
+    +   '<div class="dropdown">'
+    +      '<button class="btn btn-secondary dropdown-toggle bg-light text-dark" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'
+    +          menu.name
+    +      '</button>'
+    +      '<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">';
+    //+          '<div class="dropdown-item">Salade César <i class="bi-info-circle" data-toggle="tooltip" data-placement="top" title="sauce ketchup"></i></div>'
+    //+          '<div class="dropdown-item">Pizza Calzone <i class="bi-info-circle" data-toggle="tooltip" data-placement="top" title="XLarge"></i> <span class="badge badge-primary badge-pill badge-success">2</span></div>';
+    for(let elt of menu.elts){
+            s += gen_menu_elt(elt);
+    }
+    s+=      '</div>'
+    +   '</div>'
+    +   '<div><span class="badge badge-secondary">15€</span></div>'
+    +'</li>';
+    return s;
+}
+
 function gen_panier(){
     let p =
      '<ul id="panier" class="list-group shadow-sm">'
@@ -7,15 +51,11 @@ function gen_panier(){
         let totalPrice = 0;
         for(let elt of panier){
             totalPrice += elt.price*elt.number;
-            p +=
-             '<li class="list-group-item d-flex justify-content-between align-items-center">'
-            +   '<div>'+elt.name;
-            if(elt.choice !== undefined){
-                p+=' <i class="bi-info-circle" data-toggle="tooltip" data-placement="top" title="'+elt.choice+'"></i>'
+            if(elt.menu === true){
+                p += gen_menu(elt);
+            } else {
+                p += gen_elt(elt);
             }
-            p +=
-                '</div><div><span class="badge badge-secondary">'+elt.price+'€</span> <span class="badge badge-primary badge-pill badge-success">'+elt.number+'</span></div>'
-            +'</li>';
         }
     p +=
     '<li class="list-group-item d-flex justify-content-between align-items-center"><button type="button" class="btn btn-success">Commander</button><span class="badge badge-secondary">Total: '+totalPrice+'€</span></li>'
@@ -34,6 +74,7 @@ function init_tab(n, str) {
 
 function constr_grille(tab) {
     for (let i = 0; i < tab.length; i++) {
+        tab[i].description = "jambonne, champignonne";
         let el = gen_presentation(tab[i]);
         //let el = gen_presentation(images, tab[i], "6€");
         $("#grille").append(el);
@@ -50,7 +91,7 @@ function gen_presentation(e){
     +       '<img src="'+e.imageURL+'" class="card-img-top" alt="image de '+e.nom+'">'
     +       '<div class="card-body">'
     +       '<h5 class="card-title">'+e.nom+'</h5>'
-    +       '<p>jambon, champignon</p>'
+    +       '<p>'+e.description+'</p>'
     +       '<p id="price" class="card-text">' + (first_price) + '€</p>';
             if(multipleChoices){
                elt +='<select class="form-select mb-4" aria-label="Default select example" style="width: 100%;">'
@@ -86,18 +127,24 @@ function gen_presentation(e){
         } else {
             p = e.price;
         }
-        let o = {name: e.nom, price: p, choice: choice, number:1};
-        let added = false;
-        for(let elt of panier){
-            if(elt.name === o.name && elt.choice === o.choice){
-                elt.number++;
-                added = true;
-                break;
+        
+        if(e.menu !== true){
+            let o = {name: e.nom, price: p, menu: false, choice: choice, number:1};
+            let added = false;
+            for(let elt of panier){
+                if(elt.name === o.name && elt.choice === o.choice){
+                    elt.number++;
+                    added = true;
+                    break;
+                }
             }
+            if(!added){
+                panier.push(o);
+            }
+        } else {
+            panier.push({name: e.nom, price: p, menu: true, elts: [{name: "Salade César", price: 6, number: 2}, {name: "Pizza Calzone", price: 12, choice:"XLarge", number: 2}], number:1});
         }
-        if(!added){
-            panier.push(o);
-        }
+
         console.log(panier);
         $("#panierContainer").html(gen_panier());
         $("[data-toggle=tooltip]").tooltip();
@@ -125,6 +172,7 @@ $("document").ready(function() {
     $.get("http://localhost:8080/menus", {}, (data) => {
         console.log(data);
         constr_grille(data);
+        for(let d of data) d.menu = true;
         $("#grille").fadeIn("slow");
     });
 
@@ -133,6 +181,7 @@ $("document").ready(function() {
             $("#grille").empty();
             $.get("http://localhost:8080/menus", {}, (data) => {
                 console.log(data);
+                for(let d of data) d.menu = true;
                 constr_grille(data);
                 $("#grille").fadeIn("slow");
             });
