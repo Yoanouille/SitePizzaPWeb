7d70467d4e582941e92fbf68cb36a0699cec9b11
Yoan Rougeolle <yoyo031201@orange.fr>
2022-05-23 19:45:28 +0200

    nettoyage du fichier script.js

---
+++ init.sql
@@ -0,0 +1,2 @@
+\i sql/init_tables.sql
+\i sql/init_commandes.sql
--- public/script.js
+++ public/script.js
@@ -1,5 +1,10 @@
+//Le panier du client
 let panier = [];
 
+//Le bloc actuellement visible
+let current_block = {block:$("#grille")};
+
+//fonction qui permet d'enlever un élément du panier
 function remove_elt_panier_with_index(elt) {
     let li = elt.parent().parent();
     let ul = li.parent();
@@ -15,6 +20,7 @@ function remove_elt_panier_with_index(elt) {
     }
 }
 
+//fonction qui update le panier sur la page commande
 function update_panier() {
     $("#panierContainer").html(gen_panier(panier, false));
     $("[data-toggle=tooltip]").tooltip();
@@ -26,31 +32,85 @@ function update_panier() {
     });
 }
 
-function init_tab(n, str) {
-    let tab = [];
-    for(let i = 0; i < n; i++) {
-        tab.push(str + " " + i);
-    }
-    console.log(tab);
-    return tab;
-}
 
+//Fonction qui contruit toutes la grille de droite dans la page commande
 function constr_grille(url,tab, type, menu) {
     console.log(menu);
-    if(type === 'pizzas') {
-        console.log(type);
-        console.log(url);
-        $("#grille").append(gen_perso_card(url, menu));
-    }
+    if(type === 'pizzas') $("#grille").append(gen_perso_card(url, menu));
     for (let i = 0; i < tab.length; i++) {
         if(tab[i].menu !== undefined || menu !== undefined || tab[i].choice !== undefined) {
             let el = gen_presentation(url,tab[i], type, menu);
-            //let el = gen_presentation(images, tab[i], "6€");
             $("#grille").append(el);
         }
     }    
 }
 
+//Fonction qui génère le html de la carte du choix de la pizza personnalisée
+function gen_perso_card(url, menu) {
+    let elt =
+     '<div class="col-sm-6 col-md-4 col-lg-3">'
+    +   '<div class="card mb-4 shadow-sm img-hover">'
+    +       '<img src="images/pizza/Xlarge.jpg" class="card-img-top" alt="image perso">'
+    +       '<div class="card-body">'
+    +       '<h5 class="card-title">Pizza Personalisée</h5>'
+    +       '<p>Contruisez votre Pizza !</p>'
+    +       '<p id="price" class="card-text">A partir de 10€</p>';
+            
+    elt +=         '<button type="button" class="btn btn-success ajout" style="width: 100%;">Créer la Pizza</button>';
+    elt +=  '</div>'
+    +   '</div>'
+    +'</div>';
+
+    let el = $(elt);
+
+    //Ajout de la fonction init_perso au bouton
+    el.find("button").click(function() {
+        init_perso(url, current_block, undefined, undefined,menu);
+    });
+
+    return el;
+    
+}
+
+//Fonction qui génère le html de l'élément e qui peut être un menu/pizza/boisson/entrée
+function gen_elt_pres(e, first_price, multipleChoices,type, menu) {
+    let elt = '<div class="col-sm-6 col-md-4 col-lg-3">'
+    +   '<div class="card mb-4 shadow-sm img-hover">'
+    +       '<img src="'+e.imageURL+'" class="card-img-top" alt="image de '+e.nom+'">'
+    +       '<div class="card-body">'
+    +       '<h5 class="card-title">'+e.nom+'</h5>'
+    +       '<p>'+e.description+'</p>'
+    +       (menu === undefined ?'<p id="price" class="card-text">' + (first_price) + '€</p>' : '');
+            if(multipleChoices){
+               elt +='<select class="form-select mb-4" aria-label="Default select example" style="width: 100%;">'
+                    for(let c of e.choices){
+                        elt += '<option>'+c+'</option>';
+                    }
+                elt +='</select>';
+            }
+    elt +=         '<button type="button" class="btn btn-success ajout" style="width: 100%;">'+(menu === undefined ? (e.menu?'Composer menu':'Ajouter au panier'):'choisir')+'</button>';
+    if(type === 'pizzas') elt += '<button type="button" class="btn btn-succes personal" style="width:100%;">Personnaliser</button>';
+    elt +=  '</div>'
+    +   '</div>'
+    +'</div>';
+    return elt;
+}
+
+
+//Ajoute la fonction de selection qui change le prix sur les <select>
+function elt_select_function(el, e) {
+    el.find("select").change(function(){
+        for(let i = 0; i < e.choices.length; i++){
+            if(e.choices[i] == $(this).val()){
+                el.find("#price").text((e.price + e.prices_choices[i]) + "€");
+                break;
+            }
+        }
+    });
+}
+
+
+//Fonction qui permet de passer à la prochaine étape du menu
 function nextMenuStep(url,menu){
     menu.number++;
     let nb;
@@ -83,64 +143,8 @@ function nextMenuStep(url,menu){
     }
 }
 
-function gen_perso_card(url, menu) {
-    let elt =
-     '<div class="col-sm-6 col-md-4 col-lg-3">'
-    +   '<div class="card mb-4 shadow-sm img-hover">'
-    +       '<img src="images/pizza/Xlarge.jpg" class="card-img-top" alt="image perso">'
-    +       '<div class="card-body">'
-    +       '<h5 class="card-title">Pizza Personalisée</h5>'
-    +       '<p>Contruisez votre Pizza !</p>'
-    +       '<p id="price" class="card-text">A partir de 20€</p>';
-            
-    elt +=         '<button type="button" class="btn btn-success ajout" style="width: 100%;">Créer la Pizza</button>';
-    elt +=  '</div>'
-    +   '</div>'
-    +'</div>';
-
-    let el = $(elt);
-    el.find("button").click(function() {
-        init_perso(url, current_block, undefined, undefined,menu);
-    });
-
-    return el;
-    
-}
-
-function gen_presentation(url,e, type, menu){
-    let multipleChoices = e.choices !== undefined;
-    let first_price = e.price;
-    if(multipleChoices) first_price += e.prices_choices[0];
-    let elt =
-     '<div class="col-sm-6 col-md-4 col-lg-3">'
-    +   '<div class="card mb-4 shadow-sm img-hover">'
-    +       '<img src="'+e.imageURL+'" class="card-img-top" alt="image de '+e.nom+'">'
-    +       '<div class="card-body">'
-    +       '<h5 class="card-title">'+e.nom+'</h5>'
-    +       '<p>'+e.description+'</p>'
-    +       (menu === undefined ?'<p id="price" class="card-text">' + (first_price) + '€</p>' : '');
-            if(multipleChoices){
-               elt +='<select class="form-select mb-4" aria-label="Default select example" style="width: 100%;">'
-                    for(let c of e.choices){
-                        elt += '<option>'+c+'</option>';
-                    }
-                elt +='</select>';
-            }
-    elt +=         '<button type="button" class="btn btn-success ajout" style="width: 100%;">'+(menu === undefined ? (e.menu?'Composer menu':'Ajouter au panier'):'choisir')+'</button>';
-    if(type === 'pizzas') elt += '<button type="button" class="btn btn-succes personal" style="width:100%;">Personnaliser</button>';
-    elt +=  '</div>'
-    +   '</div>'
-    +'</div>';
-    
-    let el = $(elt);
-    el.find("select").change(function(){
-        for(let i = 0; i < e.choices.length; i++){
-            if(e.choices[i] == $(this).val()){
-                el.find("#price").text((e.price + e.prices_choices[i]) + "€");
-                break;
-            }
-        }
-    });
+//Fonction qui permet d'ajouter un élément dans le panier/ ou si c'est un menu de lancer le choix du menu
+function elt_add_function(el, url, e, multipleChoices,type, menu) {
     el.find(".ajout").click(function(){    
         let choice = el.find("select").val();
         let p = 0;
@@ -155,8 +159,12 @@ function gen_presentation(url,e, type, menu){
             p = e.price;
         }
 
+        //Si on est dans le choix d'un menu
         if(menu === undefined){
             if(!e.menu){
+                //Si c'est pas un menu on l'ajoute dans le panier
+                //Soit on incrémente le nb 
+                //Soit on l'ajoute
                 let o = {name: e.nom, price: p, menu: false, choice: choice, type: type,number:1};
                 let added = false;
                 for(let elt of panier){
@@ -166,19 +174,19 @@ function gen_presentation(url,e, type, menu){
                         break;
                     }
                 }
-                if(!added){
-                    panier.push(o);
-                }
-                console.log(panier);
+                if(!added) panier.push(o);
                 update_panier();
             } else {
+                //Si c'est un menu
+                //On lance le choix des éléments du menu
                 let menu = {name: e.nom, price: p, menu: true, elts: [], number:1, format:e, step:"entrees", number: 0};
-                console.log("MENUU");
                 get_data_and_switch(url,"entrees", menu);
                 $("#menuContainer").slideDown("slow");
                 $("#menuBar").css("left", "0%");
             }
         } else {
+            //Si on est dans le choix d'un menu
+            //On l'ajoute aux éléments du menu
             let o = {name: e.nom, price: p, type: type,choice: choice, number:1};
             let added = false;
             for(let elt of menu.elts){
@@ -188,27 +196,39 @@ function gen_presentation(url,e, type, menu){
                     break;
                 }
             }
-            if(!added){
-                menu.elts.push(o);
-            }
+            if(!added) menu.elts.push(o);
+            //On appelle la prochaine étape du menu
             nextMenuStep(url,menu);
         }
     });
+}
 
-    if(type === "pizzas") {
-        el.find(".personal").click(function() {
-            let taille = el.find("select").val();
-            $.get(url + "pizza-ingr", {pizza:e.nom}, (ingr) => {
-                console.log(ingr);
-                init_perso(url,current_block,taille, new Map(ingr), menu);
-            });
+//Fonction ajoute la fonction permettant de pouvoir personaliser ses pizzas 
+function elt_perso_function(el, url, e, menu) {
+    el.find(".personal").click(function() {
+        let taille = el.find("select").val();
+        $.get(url + "pizza-ingr", {pizza:e.nom}, (ingr) => {
+            init_perso(url,current_block,taille, new Map(ingr), menu);
         });
-    }
+    });
+}
+
+//Fonction qui génère le html d'un élement et ajoute les fonctions aux boutons
+function gen_presentation(url,e, type, menu){
+    let multipleChoices = e.choices !== undefined;
+    let first_price = e.price;
+    if(multipleChoices) first_price += e.prices_choices[0]; 
+    
+    let el = $(gen_elt_pres(e, first_price, multipleChoices, type, menu));
+    elt_select_function(el, e);
+    elt_add_function(el, url,e, multipleChoices, type, menu);
+    
+    if(type === "pizzas") elt_perso_function(el, url, e,menu);
 
     return el;
 }
 
-
+//Fonction qui permet de déplacer la bar des progress bar
 function moveBar(dir, s, bar, barText) {
     if(bar === undefined) bar = "#bar";
     if(barText === undefined) barText = "#barText";
@@ -224,9 +244,8 @@ function moveBar(dir, s, bar, barText) {
     });
 }
 
-let current_block = {block:$("#grille")};
-
-
+//Fonction qui récupère auprès du serveur certaines données
+//Génère la présentation et switch avec un effet de fade entre l'ancien et le nouveau écran
 function get_data_and_switch(url, type, menu) {
     if(menu === undefined){
         $("#menuContainer").fadeOut("slow");
@@ -240,7 +259,9 @@ function get_data_and_switch(url, type, menu) {
         }
         $("#menuStep").text(text+" "+(menu.number+1)+"/"+n);
     }
+    //requete get pour récupérer ce qu'on a besoin
     $.get(url + type, {}, (data) => {
+        //switch
         current_block.block.fadeOut("slow", function() {
             current_block.block = $("#grille");
             $("#grille").empty();
@@ -260,6 +281,7 @@ function get_data_and_switch(url, type, menu) {
                     }
                 }
             }
+            //génération de la présentation
             constr_grille(url,data, type, menu);
 
             $("#grille").fadeIn("slow");
@@ -267,9 +289,10 @@ function get_data_and_switch(url, type, menu) {
     });
 }
 
+//le main
 $("document").ready(function() {
-    console.log("COUCOU");
 
+    //récupération de l'addresse
     let url = window.location.href;
     console.log(url);
     let i = 0;
@@ -278,7 +301,7 @@ $("document").ready(function() {
     }
     url = url.substring(0, i + 1);
     
-
+    //initialisation de la page sur menu
     $.get(url + "menus", {}, (data) => {
         console.log(data);
         for(let d of data) d.menu = true;
@@ -286,11 +309,14 @@ $("document").ready(function() {
         $("#grille").fadeIn("slow");
     });
 
+    //initialisation du panier
     $("#panierContainer").html(gen_panier(panier, false));
     
+
     $("#persoContainer").hide();
     $("#menuContainer").hide();
 
+    //initialisation des boutons de nav
     $("#nav-menus").click(function() {
         get_data_and_switch(url, "menus");
     });
@@ -304,9 +330,13 @@ $("document").ready(function() {
         get_data_and_switch(url, "pizzas");
     });
 
+    //initialisation du formulaire
     valid_form(url);
 });
 
+
+//fonction qui s'active lors d'un passage de commande
+//Et si tout est bon, lance une requête post au serveur pour insérer la commande dans la bd
 function valid_form(url) {
     $("#command-form").submit(function(event) {
         event.preventDefault();
